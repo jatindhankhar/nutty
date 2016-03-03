ec80366abaad6476bde84db9c5d71f66c42f5e40
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-03-03 20:40:47 +0530

    pushing the migrations

---
+++ db/migrate/20160303150330_create_songs.rb
@@ -0,0 +1,9 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.text, :lyric
+      t.integer :upvote, default: 0
+      t.integer :downvote, default: 0
+      t.timestamps
+  end
+end
