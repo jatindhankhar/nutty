1c94c3701ea3068789af303bd9d2c6d881f74c00
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-03-04 20:47:16 +0530

    Hip hOP

--- test.rb
+++ test.rb
@@ -1,3 +1,16 @@
 require './app.rb'
-puts ENV["RACK_ENV"]
-puts Song.count
+
+def add_entry lyric,genre
+	Song.create( :lyric => lyric, :genre => genre)
+end
+
+
+files = Dir.glob ENV["OPENSHIFT_DATA_DIR"] + "*.txt" # Find all text files
+
+files.each do |f|
+	puts "For genre #{File.basename(f,".txt")
+	open(f).each_line.t_a.sample(25).each do |lyric|
+		puts lyric
+	end
+end
+
