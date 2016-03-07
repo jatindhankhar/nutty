eff049a393853109230833f7239beee31982a611
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-03-07 23:19:23 +0530

    Added ajax bases votign scheme

--- app.rb
+++ app.rb
@@ -6,38 +6,54 @@ require 'active_record'
 require 'bundler/setup'
 require 'yaml'
 
-require "./models/song.rb"
+require './models/song.rb'
 
-dbconfig = YAML.load(ERB.new(File.read("config/database.yml")).result)
+enable :sessions # For keeping track of votes for a song
+# configure(:development) { set :session_secret, "something" }
 
-# YOU MUST SET THE ENV RACK_ENV to 'production' FOR YOUR CATRIDGE
-RACK_ENV ||= ENV["RACK_ENV"] || "development"
+########## Database configuration #################
+
+dbconfig = YAML.load(ERB.new(File.read('config/database.yml')).result)
+RACK_ENV ||= ENV['RACK_ENV'] || 'development'
 ActiveRecord::Base.establish_connection dbconfig[RACK_ENV]
 
-if ENV["OPENSHIFT_RUBY_LOG_DIR"]
-	LOG_FILE = "#{ENV["OPENSHIFT_RUBY_LOG_DIR"]}/#{RACK_ENV}.log"
-	ActiveRecord::Base.logger = Logger.new(File.open(LOG_FILE, 'a+'))
+if ENV['OPENSHIFT_RUBY_LOG_DIR']
+  LOG_FILE = "#{ENV['OPENSHIFT_RUBY_LOG_DIR']}/#{RACK_ENV}.log"
+  ActiveRecord::Base.logger = Logger.new(File.open(LOG_FILE, 'a+'))
 else
-	Dir.mkdir('log') if !File.exist?('log') || !File.directory?('log')
-	ActiveRecord::Base.logger = Logger.new(File.open("log/#{RACK_ENV}.log", "a+"))
+  Dir.mkdir('log') if !File.exist?('log') || !File.directory?('log')
+  ActiveRecord::Base.logger = Logger.new(File.open("log/#{RACK_ENV}.log", 'a+'))
 end
 
+#################################################
 get '/' do
-	erb :index
-
+  session['song_id'] = nil
+  erb :index
 end
-
 
 post '/' do
-	#content_type :json
-	puts params
-	if params[:category]
-		Song.where(:genre => params[:category]).order("RANDOM()").first.lyric.to_s
-	else
-   "No such genre"
-	end
+  if params[:category]
+    res = Song.where(genre: params[:category]).order('RANDOM()').first
+    session['song_id'] = res.id
+    res.lyric.to_s
+
+  else
+    'No such genre'
+  end
 end
 
 post '/vote' do
- puts params
+  if params[:type] && session['song_id']
+    target = session['song_id'].to_i
+    puts target
+    if params[:type] == 'upvote'
+      Song.find(target).increment!(:upvote)
+      params[:type] + "d"
+    elsif target[:type] == 'downvote'
+      Song.find(target).increment!(:downvote)
+      params[:type] + "d"
+    else
+      'Type error'
+ end
+ end
 end
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,57 @@
 $(document).ready(
-function()
-{
-  $("#result").hide();
-  $("#tar").submit(function(event){
-    event.preventDefault();
-    $(this).hide();
-    console.log("category is " + $("#cat").val())
-    $.post('/',{category: $("#cat").val()},function(response,data){$("#lyric").html(response);});
-    $("#result").show();
+  function() {
+    $("#result").hide();
+    $("#vote_response").hide();
+    $("#tar").submit(function(event) {
+      event.preventDefault();
+      $(this).hide();
+      console.log("category is " + $("#cat").val())
+      $.post('/', {
+        category: $("#cat").val()
+      }, function(response, data) {
+        $("#lyric").html(response);
+      });
+      $("#result").show();
+      $("#vote").show();
 
-  });
-}
+    });
+
+    $("#again").click(
+      function() {
+        $("#tar").show();
+        $("#result").hide();
+        $("#vote_response").hide();
+
+      }
+    );
+
+    $("#tar").submit(function(event) {
+      event.preventDefault();
+      $(this).hide();
+      console.log("category is " + $("#cat").val())
+      $.post('/', {
+        category: $("#cat").val()
+      }, function(response, data) {
+        $("#lyric").html(response);
+      });
+      $("#result").show();
+
+    });
+
+    $("#vote div button").click(function(event) {
+      event.preventDefault();
+      var votes = {
+        type: $(this).val()
+      };
+      $.post('/vote', votes, function(response, data) {
+        $("#vote").hide();
+        //  $("#result").hide();
+        $("#vote_response").show();
+        $("#vote_response").html(response);
+
+      });
+
+    });
+
+  }
 );
